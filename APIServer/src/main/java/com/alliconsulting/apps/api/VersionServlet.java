4e212972de5d9089eb49c0b65c30dc3de404889d
Asif Alli <alli.asif@gmail.com>
2023-06-20 17:25:55 -0400

    Updated Version Servlet

--- APIServer/src/main/java/com/alliconsulting/apps/api/VersionServlet.java
+++ APIServer/src/main/java/com/alliconsulting/apps/api/VersionServlet.java
@@ -26,7 +26,7 @@ public class VersionServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("1.0.0");
 	}
 
 	/**
