f9e81c9a1befcb6139ce0557b501603f1ed292ea
Asif Alli <alli.asif@gmail.com>
2023-06-20 17:48:02 -0400

    Adding customizable responses based on properties

--- APIServer/src/main/java/com/alliconsulting/apps/api/CheckForUpgradeServlet.java
+++ APIServer/src/main/java/com/alliconsulting/apps/api/CheckForUpgradeServlet.java
@@ -1,11 +1,17 @@
 package com.alliconsulting.apps.api;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class CheckForUpgradeServlet
@@ -26,7 +32,27 @@ public class CheckForUpgradeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("upgrade");
+		
+		String filename = "/WEB-INF/configuration.properties";
+        ServletContext context = getServletContext();
+
+        // First get the file InputStream using ServletContext.getResourceAsStream()
+        // method.
+        InputStream is = context.getResourceAsStream(filename);
+        if (is != null) {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader reader = new BufferedReader(isr);
+            PrintWriter writer = response.getWriter();
+            String text;
+
+            // We read the file line by line and later will be displayed on the
+            // browser page.
+            while ((text = reader.readLine()) != null) {
+                writer.println(text);
+            }
+        }
+		
+		//response.getWriter().append("upgrade");
 	}
 
 	/**
